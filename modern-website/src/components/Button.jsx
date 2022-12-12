b7f6d85b856c84794f61e440c3ffffd37fabcaa7
Pannu786 <thepannu786@gmail.com>
2022-12-12 23:57:09 +0530

    #

--- modern-website/src/components/Button.jsx
+++ modern-website/src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ style }) => {
   return (
     <button
       type='button'
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${style}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] ${style}`}
     >
       Get Started
     </button>
